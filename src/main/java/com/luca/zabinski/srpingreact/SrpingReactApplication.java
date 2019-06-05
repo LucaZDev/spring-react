5029e746d818474a9873ae24b96b0be250a0cc94
LucaZDev <luca.zabinski.dev@gmail.com>
2019-06-05 23:31:41 +0200

    Adding Spring boot project to repo

---
+++ src/main/java/com/luca/zabinski/srpingreact/SrpingReactApplication.java
@@ -0,0 +1,13 @@
+package com.luca.zabinski.srpingreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpingReactApplication.class, args);
+	}
+
+}
